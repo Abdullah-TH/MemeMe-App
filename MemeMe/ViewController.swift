29efdd927b6e1cd4a2dd4bdd9080d1cbdca469fd
Abdullah Althobetey <abdullah.th@me.comm>
2016-12-13 07:57:35 +0300

    Implement the top and bottom text fields Implement sliding the view up when the keyboard appear, and sliding it down when the keyboard disappear

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate
 {
+    // MARK: Outlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextFeild: UITextField!
     @IBOutlet weak var bottomTextFeild: UITextField!
+    
+    // MARK: Properties
+    let memeTextAttributes: [String: Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: -5.0
+    ]
 
+    // MARK: ViewController Life Cycle
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        topTextFeild.defaultTextAttributes = memeTextAttributes
+        topTextFeild.textAlignment = .center
+        topTextFeild.delegate = self
+        
+        bottomTextFeild.defaultTextAttributes = memeTextAttributes
+        bottomTextFeild.textAlignment = .center
+        bottomTextFeild.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+    
+    
+    // MARK: Actions
     
     @IBAction func pickPhoto(_ sender: Any)
     {
@@ -37,6 +72,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: Keyboard Notifications
+    
+    func subscribeToKeyboardNotifications()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications()
+    {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(_ notification: Notification)
+    {
+        self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification: Notification)
+    {
+        self.view.frame.origin.y += getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat
+    {
+        let keyboardSize = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
     // MARK: UIImagePickerControllerDelegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
@@ -48,5 +113,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: UITextFeildDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField)
+    {
+        if textField === topTextFeild
+        {
+            if textField.text == "TOP"
+            {
+                textField.text = ""
+            }
+        }
+        else if textField === bottomTextFeild
+        {
+            if textField.text == "BOTTOM"
+            {
+                textField.text = ""
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
